801ad37cc25c8d888b56f95f888533a62aca1ec2
arpitatwork <arpitverma20202020@gmail.com>
2024-05-14 12:14:10 +0530

    Initial Commit

---
+++ githubDemo/githubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubDemoApp.swift
+//  githubDemo
+//
+//  Created by arpit verma on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct githubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
